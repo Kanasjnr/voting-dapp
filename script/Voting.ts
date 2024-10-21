57256a2b1ad537464a6bbb1d99a0758f813bcc3b
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2024-10-21 12:31:06 +0100

    Script added

--- script/Voting.ts
+++ script/Voting.ts
@@ -4,8 +4,12 @@ async function main() {
   const contractAddress = "0x2156842Af186bA8B9B232d4dB58ADDb016969FE5";
 
   const account = "0xF1240B5C1C468aA68Bd77DCFAf10d6d46E9CB8Ea";
+  const account2= "0xF3709C87432488B6aAEb9629cf5cb5BA6Db793F0"
+  const account3 = "0x1853E7DE95130a304e4dF355CF8aB7AE80160189"
 
   const signer = await hre.ethers.getSigner(account);
+  const signer1 = await hre.ethers.getSigner(account2);
+  const signer2 = await hre.ethers.getSigner(account3);
 
   const proposalVoting = await hre.ethers.getContractAt(
     "ProposalVote",
@@ -17,12 +21,13 @@ async function main() {
     .createProposal(
       "propose to cancel test",
       "i think we should cancel the test",
-      4
+      2
     );
 
   await createProposal.wait();
 
   console.log("proposal created successfully");
+  
 
   const votingOnProposal = await proposalVoting
     .connect(signer)
@@ -30,7 +35,20 @@ async function main() {
 
   await votingOnProposal.wait();
 
+  const votingOnProposal_ = await proposalVoting
+    .connect(signer1)
+    .voteOnProposal(0);
+
+  await votingOnProposal_.wait();
+  const _votingOnProposal = await proposalVoting
+    .connect(signer2)
+    .voteOnProposal(0);
+
+  await _votingOnProposal.wait();
+
   console.log("You have voted on proposal");
+
+
 
   const allCreatedProposals = await proposalVoting.getAllProposals();
 
