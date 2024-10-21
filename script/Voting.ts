6bfb3e31e3a0d8e58001f95619e17e4fd6f5b9e0
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2024-10-21 11:23:18 +0100

    update

--- script/Voting.ts
+++ script/Voting.ts
@@ -1,49 +1,11 @@
-import { ethers } from "hardhat";
+import hre from "hardhat";
 
-async function main() {
-  // Deploy the contract
-  const ProposalVote = await ethers.getContractFactory("ProposalVote");
-  const proposalVote = await ProposalVote.deploy();
-  await proposalVote.deployed();
-  console.log("ProposalVote deployed to:", proposalVote.address);
+async function main(){
+    const contractAddress = "0x2156842Af186bA8B9B232d4dB58ADDb016969FE5";
 
-  // Create a proposal
-  const createProposalTx = await proposalVote.createProposal(
-    "First Proposal",
-    "This is a test proposal",
-    5 // quorum
-  );
-  await createProposalTx.wait();
-  console.log("Proposal created");
+    const account = "0xF1240B5C1C468aA68Bd77DCFAf10d6d46E9CB8Ea";
 
-  // Vote on the proposal
-  const voteOnProposalTx = await proposalVote.voteOnProposal(0);
-  await voteOnProposalTx.wait();
-  console.log("Voted on proposal");
+    const signer = await hre.ethers.getSigner(account);
 
-  // Get all proposals
-  const allProposals = await proposalVote.getAllProposals();
-  console.log("All proposals:", allProposals);
-
-  // Get a specific proposal
-  const proposal = await proposalVote.getProposal(0);
-  console.log("Specific proposal:", proposal);
-
-  // Vote multiple times to reach quorum
-  for (let i = 1; i < 5; i++) {
-    const signer = (await ethers.getSigners())[i];
-    await proposalVote.connect(signer).voteOnProposal(0);
-    console.log(`Signer ${i} voted on proposal`);
-  }
-
-  // Check the proposal status after reaching quorum
-  const updatedProposal = await proposalVote.getProposal(0);
-  console.log("Updated proposal after reaching quorum:", updatedProposal);
+    const proposalVoting = await hre.ethers.getContractAt("ProposalVote", contractAddress)
 }
-
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
